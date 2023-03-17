31686b5906e5c9ec885f89712e926e05df3797f1
Chandru Vinayagam <rajuashwin1410@gmail.com>
2023-03-17 23:16:50 +0530

    logout btn

--- wpm-final/navbar.php
+++ wpm-final/navbar.php
@@ -1,6 +1,17 @@
 <nav class="navbar navbar-light bg-light p-3">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="index.php">
         <img src="assets/img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="" />
         Word Per Minute - Calculator
     </a>
+    <?php
+    if (isset($_SESSION['user_session'])) {
+        ?>
+    <div>
+        <span class="navbar-text">
+            <a href="logout.php">LOGOUT</a>
+        </span>
+    </div>
+    <?php
+    }
+    ?>
 </nav>
