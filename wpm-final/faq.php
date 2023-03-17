76147deddee9034d9f6641e5c307b8d6d700edd0
Chandru Vinayagam <rajuashwin1410@gmail.com>
2023-03-17 23:34:08 +0530

    content change

--- wpm-final/faq.php
+++ wpm-final/faq.php
@@ -4,19 +4,17 @@
             <h5 class="mb-0">
                 <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
                     aria-controls="collapseOne">
-                    Collapsible Group Item #1
-                </button>
+                    How do I increase my typing speed? </button>
             </h5>
         </div>
 
         <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
             <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-                moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-                proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-                aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                There are two ways to type faster: The best way to increase typing speed is to learn to type the correct
+                way. “Touch typing” means you are able to type with all 10 fingers instead of using a “hunt and peck”
+                method of typing. You can learn how to touch type with Typing.com’s free typing lessons. The second way
+                you can learn to type faster is by playing typing games. Keyboard games like Nitro Type can help you
+                practice your typing speed and increase your words per minute score.
             </div>
         </div>
     </div>
@@ -25,19 +23,14 @@
             <h5 class="mb-0">
                 <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="false" aria-controls="collapseTwo">
-                    Collapsible Group Item #2
+                    How is typing speed measured?
                 </button>
             </h5>
         </div>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
             <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-                moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-                proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-                aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-            </div>
+                Typing speed is measured by the number of words you can type correctly in a set amount of time. A “word”
+                is equivalent to five keystrokes. </div>
         </div>
     </div>
     <div class="card">
@@ -45,19 +38,15 @@
             <h5 class="mb-0">
                 <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree"
                     aria-expanded="false" aria-controls="collapseThree">
-                    Collapsible Group Item #3
-                </button>
+                    Why is it important to take a typing speed test? </button>
             </h5>
         </div>
         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
             <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-                moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-                proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-                aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-            </div>
+                Taking a typing speed test establishes your average typing speed (WPM) and accuracy, which is an
+                important baseline to know so you can increase speed and improve accuracy with practice. Periodically
+                taking typing speed tests can help you track your progress and measure improvement. You even can use
+                your WPM score from the typing test on your resume to highlight your administrative skills </div>
         </div>
     </div>
 </div>
